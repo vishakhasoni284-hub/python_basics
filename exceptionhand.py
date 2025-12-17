48d1d9363c0b3f33ebc4ab67b235df6b13c78f31
vishakhasoni284-hub <vishakhasoni284@gmail.com>
2025-12-17 13:02:37 +0530

    Exception handling
    This file includes some codes related to exception handling.

---
+++ exceptionhand.py
@@ -0,0 +1,32 @@
+'''try:
+    print("this is 1st line .")
+    print(z)
+    print(a/10)
+    print("this is 2nd line .")
+    print("this is 2nd line .")
+except (NameError,IndexError) as e :
+    print("error handling ",e)'''
+
+'''try:
+    print("this is 1st line .")
+    try:
+        print(z)
+    except:
+        print("Inner exception")
+    
+    print("this is 2nd line .")
+except (NameError,IndexError) as e :
+    print("error handling ",e)'''
+
+'''try:
+    print("this is 1st line .")
+    print(z)
+    print("this is 2nd line .")
+except'''
+
+
+
+'''try:
+    n=0
+    res=100/n
+except ZeroDivisionError:'''
